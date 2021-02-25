b86a8c98db8451e885d3f7ed1866230acf69eae7
Kartik Jain <kartikhtog@gmail.com>
2021-02-25 08:12:10 -0800

    Inital android project push android min 8.0, using kotlin

---
+++ BluetoothAutoConnectSetting/app/src/main/java/com/example/bluetoothautoconnectsetting/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.bluetoothautoconnectsetting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(applicationContext,"coming soon", Toast.LENGTH_SHORT).show()
+    }
+}
